f2d3abbc4e19e33893940ce52aa9dd2b79c9f3c4
ItzNesbroDev <asnesbeer3@gmail.com>
2023-05-05 18:37:56 +0700

    feat(fish): added useful functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,8 +17,8 @@ alias ll "ls -l"
 alias lla "ll -A"
 alias g git
 alias tmp "cd /tmp"
-alias cl "clear"
-alias cat "batcat"
+alias cl clear
+alias cat batcat
 
 set -gx EDITOR nvim
 
@@ -26,6 +26,7 @@ set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
 set -gx PATH ~/.local/share/nvm/*/bin $PATH
+set -gx PATH ~/.local/share/bob/nvim-bin $PATH
 
 # NodeJS
 set -gx PATH node_modules/.bin $PATH
@@ -39,29 +40,30 @@ set -gx PATH $GOPATH/bin $PATH
 
 # NVM
 function __check_rvm --on-variable PWD --description 'Do nvm stuff'
-  status --is-command-substitution; and return
+    status --is-command-substitution; and return
 
-  if test -f .nvmrc; and test -r .nvmrc;
-    nvm use
-  else
-  end
+    if test -f .nvmrc; and test -r .nvmrc
+        nvm use
+    else
+    end
 end
 
 switch (uname)
-  case Darwin
-    source (dirname (status --current-filename))/config-osx.fish
-  case Linux
-    source (dirname (status --current-filename))/config-linux.fish
-  case '*'
-    source (dirname (status --current-filename))/config-windows.fish
+    case Darwin
+        source (dirname (status --current-filename))/config-osx.fish
+    case Linux
+        source (dirname (status --current-filename))/config-linux.fish
+    case '*'
+        source (dirname (status --current-filename))/config-windows.fish
 end
 
 set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $LOCAL_CONFIG
-  source $LOCAL_CONFIG
+    source $LOCAL_CONFIG
 end
 
-set -x N_PREFIX "$HOME/n"; contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+set -x N_PREFIX "$HOME/n"
+contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin" # Added by n-install (see http://git.io/n-install-repo).
 
 # pnpm
 set -gx PNPM_HOME "/home/itznesbro/.local/share/pnpm"
@@ -74,5 +76,45 @@ set -gx PATH "$DENO_INSTALL" $PATH
 
 # Mason.nvim
 set -gx PATH "/home/itznesbro/.local/share/nvim/mason/bin" $PATH
+
+# Neovim Config Switcher
+set -x items "nvim\nLazyVim"
+
+function nvims
+    set selected_value (echo -e $items | fzf --select-1)
+    if [ -n "$selected_value" ]
+        set -x NVIM_APPNAME $selected_value
+        nvim
+    end
+end
+
+# Define function to switch git branches using fzf
+function gco
+  # Get the list of all git branches and pass them to fzf
+  set branch (git branch -a | sed 's/^[* \t]*//' | fzf)
+
+  # Check if a branch was selected
+  if test -n "$branch"
+    # Switch to the selected branch using git checkout
+    git checkout $branch
+  end
+end
+
+abbr -a gco 'gco'
+
+function ghqcd
+  # Get the list of directories and pass them to fzf
+  set -l ghq_dir ~/.ghq/github.com/ItzNesbroDev/
+  set -l target (find $ghq_dir -type d -maxdepth 1 -mindepth 1 -exec basename {} \; | fzf)
+
+  # Check if a directory was selected
+  if test -n "$target"
+    # Change to the selected directory
+    cd "$ghq_dir$target"
+  end
+end
+
+# Create an abbreviation for the function
+abbr -a fcd 'ghqcd'
 
 export OPENAI_API_KEY=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
