ba3843fbbf7a85486964bae09e01527e63410a70
ItzNesbroDev <asnesbeer3@gmail.com>
2023-05-19 23:33:23 +0700

    feat(nvim): added oil.nvim plugin | disabled neo-tree

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   { "folke/noice.nvim", enabled = false },
-  { "windwp/nvim-spectre", enabled = false }
+  { "windwp/nvim-spectre", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false}
 }
---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup()
+    vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+  end
+}
