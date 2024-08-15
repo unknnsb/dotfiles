9876e81d44082b5f5acdbd6263e57f6439c37fdf
ItzNesbroDev <asnesbeer3@gmail.com>
2024-08-15 10:40:46 +0530

    fix: autotag

--- .config/nvim/lua/itznesbro/plugins/treesitter.lua
+++ .config/nvim/lua/itznesbro/plugins/treesitter.lua
@@ -12,9 +12,6 @@ return {
           enable = true,
           additional_vim_regex_highlighting = false,
         },
-        autotag = {
-          enable = true
-        }
       }
     end
   },
